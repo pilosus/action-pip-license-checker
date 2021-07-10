0dde3bb40e8b27f125c76d1925f018c22c00ba83
Vitaly Samigullin <vrs@pilosus.org>
2021-07-11 00:36:12 +0300

    Fix multiline output formatting

---
+++ CHANGELOG.md
@@ -0,0 +1,23 @@
+# Change Log
+
+All notable changes to this project will be documented in this file.
+This change log follows the conventions of [keepachangelog.com](http://keepachangelog.com/).
+
+## [Unreleased]
+### Fixed
+- Nothing here yet
+
+## [0.1.1] - 2021-07-10
+### Added
+- Changelog added
+
+### Fixed
+- Multiline output string formatting fixed as per the [workaround](https://github.community/t/set-output-truncates-multiline-strings/16852)
+
+## 0.1.0 - 2021-07-10
+### Added
+- MVP
+
+
+[Unreleased]: https://github.com/pilosus/pip-license-checker/compare/0.1.1...HEAD
+[0.1.1]: https://github.com/pilosus/pip-license-checker/compare/0.1.0...0.1.1
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+# working directory is plugged as docker volume
 workdir="/github/workspace"
+
+# use all specified options
 cmd="--requirements ${workdir}/$1"
 
 if [ ! -z "$2" ] ; then
@@ -27,9 +30,17 @@ if [ ! -z "$7" ] ; then
     cmd="${cmd} --table-headers"
 fi
 
+# run command and save its exit code
 echo "Running command: java -jar /usr/src/app/app.jar ${cmd}"
 report=$( java -jar /usr/src/app/app.jar $cmd)
 status=$?
+
+# ugly formatting to make multi-line string work
+# https://github.community/t/set-output-truncates-multiline-strings/16852
+report="${report//'%'/'%25'}"
+report="${report//$'\n'/'%0A'}"
+report="${report//$'\r'/'%0D'}"
+
 echo "::set-output name=report::$report"
 
 if [ "$status" -eq "0" ] ; then
